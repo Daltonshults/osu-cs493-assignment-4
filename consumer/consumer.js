80ed7f5e4abcbd125370a1bfccbcbccc40b08129
Daltonshults <Dshults@cocc.edu>
2024-05-31 13:59:10 -0700

    Connecting to the database fixed the errors.

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -1,23 +1,50 @@
 const amqp = require('amqplib');
-const { getDbReference } = require('./mongo.js');
-const { GridFSBucket } = require('mongodb');
+const { getDbReference, connectToDb } = require('./mongo.js');
+const { GridFSBucket, ObjectId, Collection } = require('mongodb');
 const rabbitmqHost =  'rabbitmq';  //process.env.RABBITMQ_HOST;
 const rabbitmqUrl = `amqp://${rabbitmqHost}`;
-
+const sharp = require('sharp');
+const fs = require('fs');
+const { connect } = require('http2');
 async function main() {
+  
   try {
-    console.log(`TRYING FROM CONSUMER.JS`);
-    const connection = await amqp.connect(rabbitmqUrl);
-    const channel = await connection.createChannel();
-    await channel.assertQueue('photos');
-    channel.consume('photos', (msg) => {
-        if (msg) {
-          const db = getDbReference();
-          const bucket = new GridFSBucket(db, {bucketName: 'photos'});
-          const image_id = msg.content.toString();
-          console.log(`\n\n\n\n\nIMAGE ID: ${image_id}\n\n\n\n\n`);
-          channel.ack(msg);
-        }
+    connectToDb(async function () {
+      console.log(`== Consumer connected to db`);
+      const connection = await amqp.connect(rabbitmqUrl);
+      const channel = await connection.createChannel();
+      const db = await getDbReference();
+      await channel.assertQueue('photos');
+      channel.consume('photos', async (msg) => {
+          if (msg) {
+            const filesCollection = new Collection(db, 'photos.files');
+            const bucket = new GridFSBucket(db, {bucketName: 'photos'});
+            const image_id = ObjectId(msg.content.toString());
+            const downloadStream = bucket.openDownloadStream(image_id);
+            const thumbnailStream = sharp().resize(100, 100).png();
+            const thumbnailBucket = new GridFSBucket(db, {bucketName: 'thumbnails'});
+            const thumbId = new ObjectId();
+            const thumbnailMeta = {
+              imageId: image_id,
+              contentType: 'image/jpeg'
+            };
+            const uploadStream = thumbnailBucket.openUploadStreamWithId(
+              thumbId,
+              `${image_id}.jpg`,
+              { metadata: thumbnailMeta });
+
+            downloadStream.pipe(thumbnailStream).pipe(uploadStream);
+
+            uploadStream.on('finish', async function() {
+              await filesCollection.updateOne(
+                { _id: image_id },
+                { $set: { 'metadata.thumbId' : thumbId } }
+              );
+
+            });
+
+            channel.ack(msg);
+          }});
       });
   } catch (err) {
     console.error(err);
